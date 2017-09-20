8a1cd719f609a68b5232b747b305dcf8432270de
Freshwater <wfresh1180@greenville.k12.sc.us>
2017-09-20 09:27:11 -0400

    Reformatted code and added a title and credits screen

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,40 +2,92 @@ import random
 
 # config
 low = 1
-high = 100
-limit = 10
+high = 10
+limit = 4
 
-# start game
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+# helper functions
+def show_start_screen():
+    print("   _____                                  _   _                 _                 ")     
+    print("  / ____|                         /\     | \ | |               | |                ")
+    print(" | |  __ _   _  ___  ___ ___     /  \    |  \| |_   _ _ __ ___ | |__   ___ _ __   ")
+    print(" | | |_ | | | |/ _ \/ __/ __|   / /\ \   | . ` | | | | '_ ` _ \| '_ \ / _ \ '__|  ")
+    print(" | |__| | |_| |  __/\__ \__ \  / ____ \  | |\  | |_| | | | | | | |_) |  __/ |     ")
+    print("  \_____|\__,_|\___||___/___/ /_/    \_\ |_| \_|\__,_|_| |_| |_|_.__/ \___|_|     ")  
 
-guess = -1
-tries = 0
-
-#helper functions
+def show_credits():
+    print("This awesome game was created by")
+    print("$$\      $$\                     $$\                           $$$$$$$$\                            $$\                                $$\                         ") 
+    print("$$ | $\  $$ |                    $$ |                          $$  _____|                           $$ |                               $$ |                        ") 
+    print("$$ |$$$\ $$ | $$$$$$\   $$$$$$$\ $$ | $$$$$$\  $$\   $$\       $$ |    $$$$$$\   $$$$$$\   $$$$$$$\ $$$$$$$\  $$\  $$\  $$\  $$$$$$\ $$$$$$\    $$$$$$\   $$$$$$\  ") 
+    print("$$ $$ $$\$$ |$$  __$$\ $$  _____|$$ |$$  __$$\ $$ |  $$ |      $$$$$\ $$  __$$\ $$  __$$\ $$  _____|$$  __$$\ $$ | $$ | $$ | \____$$\\_$$  _|  $$  __$$\ $$  __$$\ ")
+    print("$$$$  _$$$$ |$$$$$$$$ |\$$$$$$\  $$ |$$$$$$$$ |$$ |  $$ |      $$  __|$$ |  \__|$$$$$$$$ |\$$$$$$\  $$ |  $$ |$$ | $$ | $$ | $$$$$$$ | $$ |    $$$$$$$$ |$$ |  \__|")
+    print("$$$  / \$$$ |$$   ____| \____$$\ $$ |$$   ____|$$ |  $$ |      $$ |   $$ |      $$   ____| \____$$\ $$ |  $$ |$$ | $$ | $$ |$$  __$$ | $$ |$$\ $$   ____|$$ |      ") 
+    print("$$  /   \$$ |\$$$$$$$\ $$$$$$$  |$$ |\$$$$$$$\ \$$$$$$$ |      $$ |   $$ |      \$$$$$$$\ $$$$$$$  |$$ |  $$ |\$$$$$\$$$$  |\$$$$$$$ | \$$$$  |\$$$$$$$\ $$ |      ") 
+    print("\__/     \__| \_______|\_______/ \__| \_______| \____$$ |      \__|   \__|       \_______|\_______/ \__|  \__| \_____\____/  \_______|  \____/  \_______|\__|      ") 
+    print("                                               $$\   $$ |                                                                                                          ") 
+    print("                                               \$$$$$$  |                                                                                                          ") 
+    print("                                                \______/                                                                                                           ") 
+    
 def get_guess():
     while True:
-        g = input("Take a guess: ")
+        guess = input("Guess a number: ")
 
-        if g.isnumeric():
-            g = int(g)
-            return g
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
         else:
             print("You must enter a number.")
-        
-# play the game
-while guess != rand and tries < limit:
-    guess = get_guess()
-    
+
+def pick_number():
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+
+    return random.randint(low, high)
+
+def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
 
-    tries += 1
+def show_result(guess, rand):
+    if guess == rand:
+        print("You win!")
+    else:
+        print("You are such a loser! The number was " + str(rand) + ".")
 
-# tell player outcome
-if guess == rand:
-    print ("You win!")
-else:
-    print ("You lose. The number I was thinking of was " + str(rand) + ".")
+def play_again():
+    while True:
+        decision = input("Would you like to play again? (y/n) ")
+
+        if decision == 'y' or decision == 'yes':
+            return True
+        elif decision == 'n' or decision == 'no':
+            return False
+        else:
+            print("I don't understand. Please enter 'y' or 'n'.")
+
+def play():
+    guess = -1
+    tries = 0
+
+    rand = pick_number()
+    
+    while guess != rand and tries < limit:
+        guess = get_guess()
+        check_guess(guess, rand)
+
+        tries += 1
+
+    show_result(guess, rand)
+
+
+# Game starts running here
+show_start_screen()
+
+playing = True
+
+while playing:
+    play()
+    playing = play_again()
+
+show_credits()
